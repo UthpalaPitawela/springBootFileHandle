5c2a753ba2bdc94217ec9b980287040a2b834c16
UthpalaPitawela <uthpala.isiru@gmail.com>
2018-12-21 15:09:28 +0530

    Change terminal output using StringJoiner

--- src/main/java/com/kaveesha/webApplication/Controllers/RestApiController.java
+++ src/main/java/com/kaveesha/webApplication/Controllers/RestApiController.java
@@ -15,6 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 @RestController
 public class RestApiController {
@@ -33,7 +34,7 @@ public class RestApiController {
 //        ProcessBuilder p = new ProcessBuilder(command);
 
         String resp = "";
-        ProcessBuilder builder = new ProcessBuilder("/home/kaveeshab/Dev/Kavee Dev/Scripts/script.sh", id, id);
+        ProcessBuilder builder = new ProcessBuilder("/home/uthpala/Uthpala/Uthpala/4th year/research/Shell cmd example/script.sh", id, id);
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -76,7 +77,8 @@ public class RestApiController {
     public ResponseEntity<Object> singleFileUpload(@RequestParam("file") MultipartFile file) {
          String resp = "";
          String command = "123";
-         String UPLOADED_FOLDER = "/home/kaveeshab/Dev/Kavee Dev/Scripts";
+        String result = "";
+         String UPLOADED_FOLDER = "/home/uthpala/Uthpala/Uthpala/4th year/research/Shell cmd example/";
         if (file.isEmpty()) {
             Map<String, String> stringStringMap = new HashMap<>();
             stringStringMap.put("status", "failed");
@@ -90,15 +92,24 @@ public class RestApiController {
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            System.out.println("File name is :- "+ file.getOriginalFilename() );
+            System.out.println("File name is :- "+ path);
             Files.write(path, bytes);
 
+
            // ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "cd \"D:\\temp\"  && tesseract "+file.getOriginalFilename()+" stdout -l eng");
-            ProcessBuilder builder = new ProcessBuilder("/home/kaveeshab/Dev/Kavee Dev/Scripts/script.sh", command, command);
+
+           // ProcessBuilder builder = new ProcessBuilder("/home/uthpala/Uthpala/Uthpala/4th year/research/Shell cmd example/script.sh", command, command);
+            ProcessBuilder builder = new ProcessBuilder("/home/uthpala/Uthpala/Uthpala/DockerVolume/BlockSci/docker/taint_run.sh");
             builder.redirectErrorStream(true);
             Process p = builder.start();
             BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+            r.lines().iterator().forEachRemaining(sj::add);
+            result = sj.toString();
             String line;
+            System.out.println("!!!!!!!!!!!!!!!"+ result+ "`````````````");
+
+
             while (true) {
                 line = r.readLine();
                 if (line == null) {
@@ -106,7 +117,6 @@ public class RestApiController {
                 }
                 resp = resp + System.lineSeparator() + line ;
                 System.out.println(line);
-
             }
 
         } catch (IOException e) {
@@ -114,14 +124,14 @@ public class RestApiController {
             Map<String, String> stringStringMap = new HashMap<>();
             stringStringMap.put("status", "failed");
             stringStringMap.put("message", "not  deleted");
-            stringStringMap.put("code", "200");
+            stringStringMap.put("code", "400");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).
                     body(stringStringMap);
         }
 
             Map<String, String> stringStringMap = new HashMap<>();
             stringStringMap.put("status", "success");
-            stringStringMap.put("message", resp);
+            stringStringMap.put("message", result);
             stringStringMap.put("code", "200");
             return ResponseEntity.status(HttpStatus.OK).
                     body(stringStringMap);
